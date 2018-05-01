f43568acc36a5a716a778aefb840d833b573aff2
wade welch <wade.c.welch@gmail.com>
2018-05-01 14:09:25 -0500

    added comments

--- add2.py
+++ add2.py
@@ -1,7 +1,9 @@
+#define function that adds 2 numbers
 def addNumbers(num1, num2):
     return num1 + num2
 
-
+#take the user input
 number1 = float(input('enter first number '))
 number2 = float(input('enter second number '))
+#call the function and print
 print(addNumbers(number1,number2))
