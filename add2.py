87912ce63e751d043d85cd004e553fa68febc4d1
wade welch <wade.c.welch@gmail.com>
2018-05-01 12:18:31 -0500

    adds 2 numbers first try

---
+++ add2.py
@@ -0,0 +1,7 @@
+def addNumbers(num1, num2):
+    return num1 + num2
+
+
+number1 = float(input('enter first number '))
+number2 = float(input('enter second number '))
+print(addNumbers(number1,number2))
